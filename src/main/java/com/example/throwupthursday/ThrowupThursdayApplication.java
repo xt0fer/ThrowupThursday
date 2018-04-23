5840105af2444162f1dfb4f64f36c913668577ab
KrYounger <kryounger@gmail.com>
2018-04-23 15:20:50 -0400

    postgres jpa

--- src/main/java/com/example/throwupthursday/ThrowupThursdayApplication.java
+++ src/main/java/com/example/throwupthursday/ThrowupThursdayApplication.java
@@ -2,10 +2,12 @@ package com.example.throwupthursday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ThrowupThursdayApplication {
 
 	public static void main(String[] args) {
